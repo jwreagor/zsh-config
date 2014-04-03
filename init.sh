9b65e8e0f67cb92c6108b7b78304459bc4086242
Justin Reagor <justinwr@gmail.com>
2014-04-03 13:48:51 -0400

    start initialization script

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+#
+# it is not necessary to change
+# survival is not mandatory
+#
+ln -s $HOME/.zsh/zshrc $HOME/.zshrc
+ln -s $HOME/.zsh/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.zsh/pryrc $HOME/.pryrc
+ln -s $HOME/.zsh/irbrc $HOME/.irbrc
+ln -s $HOME/.zsh/gitignore $HOME/.gitignore
+ln -s $HOME/.zsh/gitconfig $HOME/.gitconfig
+ln -s $HOME/.zsh/ccl-init.lisp $HOME/.ccl-init.lisp
+ln -s $HOME/.zsh/ackrc $HOME/.ackrc
+ln -s $HOME/.zsh/mpd $HOME/.mpd
+ln -s $HOME/.zsh/ncmpcpp $HOME/.ncmpcpp
