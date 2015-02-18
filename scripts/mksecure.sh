63227a0095a8b9296634752c09e29dd9ee725073
Justin Reagor <justinwr@gmail.com>
2015-02-18 15:50:20 -0500

    Couple changes to mksecure

--- scripts/mksecure.sh
+++ scripts/mksecure.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env zsh
 
-echo "Creating secure fs"
+local __name=${1:-secure_disk}
+local __file=${__name}.dmg
+local __size=500m
+local __enc=AES-256
+local __fs=HFS+
+local __path=~/Desktop/${__name}
+local __dmg=~/Desktop/${__file}
+local __mnt=/Volumes/${__name}
+
+echo "Creating encrypted FS \"$__dmg\""
+echo
+
 hdiutil create \
   -autostretch \
   -attach \
   -stdinpass \
-  -encryption AES-256 \
-  -size 500m \
-  -fs HFS+ \
-  -volname ${1:-secure_disk} \
-  ~/Desktop/${1:-secure_disk}
+  -encryption $__enc \
+  -size $__size \
+  -fs $__fs \
+  -volname $__name \
+  $__path
+
+if [ -d $__mnt ]; then
+  echo
+  echo "Mounted at \"$__mnt\""
+  cd $__mnt
+fi
+
